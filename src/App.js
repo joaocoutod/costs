d46cb5f94e13414de2da26a941d9898d218cf14a
João Lucas <joaocoutod@gmail.com>
2023-05-11 22:51:06 -0400

    estrutura de paginas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+
+import Home from "./pages/Home";
+import Contato from "./pages/Contato";
+import Company from "./pages/Company"
+import NewProject from "./pages/NewProject"
+
+import Container from "./layouts/Container";
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/contato">Contato</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/newproject">New Project</Link>
+      </ul>
+
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home></Home>}></Route>
+          <Route exact path="/contato" element={<Contato></Contato>}></Route>
+          <Route exact path="/company" element={<Company></Company>}></Route>
+          <Route exact path="/newproject" element={<NewProject></NewProject>}></Route>
+        </Routes>
+      </Container>
+
+      <p>Footer</p>
+    </Router>
   );
 }
 
